6d381163c98b173a97878020f1c145decc5e4e80
Rodrigo Otero <rodri.m3n@gmail.com>
2015-12-06 23:30:29 -0300

    adds some scopes for bills

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -3,4 +3,9 @@ class Bill < ActiveRecord::Base
   belongs_to :client
   validates :description, :total, :emission_date, presence: true
   validates :total, numericality: true
+
+  scope :by_year, lambda {|date| where("emission_date >= ? and emission_date <= ?", "#{date.year}-01-01", "#{date.year}-12-31")}
+  scope :per_month, lambda {|date| where("emission_date >= ? and emission_date <= ?", "#{date.year}-01-01", "#{date.year}-12-31").group("strftime('%m', emission_date)").count }
+  scope :most_sold, -> { group(:person_id).order('count_id desc').count(:id) }
+
 end
