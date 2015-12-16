bfc13b78a2c14269e30df843fcbfbf6b27d8b697
Rodrigo Otero <rodri.m3n@gmail.com>
2015-12-16 04:10:37 -0300

    adds some weird tests ha

--- test/models/bill_test.rb
+++ test/models/bill_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class BillTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "creacion CON errores de validacion" do
+    bill = Bill.create(description: 'una descripcion',
+                total: '',
+                emission_date: '2015-09-09',
+                person_id: 1,
+                client_id: 1,
+                )
+    assert_not_empty bill.errors.full_messages
+  end
+
+  test "creacion SIN errores de validacion" do
+    bill = Bill.new(description: 'una descripcion',
+                total: 100,
+                emission_date: '2015-09-09',
+                person_id: 1,
+                client_id: 1,
+                )
+    assert bill.save
+  end
 end
--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,7 +1,75 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @client = Client.create(name: 'Juan',
+                        surname: 'Carlos',
+                        gender: 'Hombre',
+                        dni: 123456,
+                        birthdate: '2015-09-09',
+                        cui: 9999999,
+                        contacts_attributes: [ source: 'skype', value: 'juan.carlos' ]
+                        )
+
+    @person = Person.create(name: 'Roberto', cui: 929292918231)
+
+    @bill = Bill.create(description: 'una descripcion',
+                total: 100,
+                emission_date: '2015-09-09',
+                person_id: @person.id,
+                client_id: @client.id,
+                )
+  end
+
+  def teardown
+    @client = nil
+  end
+
+  test "creacion con DNI no valido" do
+    cliente = Client.create(name: 'Juan',
+                        surname: 'Carlos',
+                        gender: 'Hombre',
+                        birthdate: '2015-09-09',
+                        cui: 98127364,
+                        dni: 'un_string',
+                        )
+
+    assert_not_empty cliente.errors.full_messages
+  end
+
+  test "creacion SIN dato de contacto" do
+    cliente = Client.create(name: 'Juan',
+                        surname: 'Carlos',
+                        gender: 'Hombre',
+                        dni: 995599,
+                        birthdate: '2015-09-09',
+                        cui: 81249475,
+                        )
+    assert_empty cliente.contacts
+  end
+
+  test "creacion CON dato de contacto" do
+    cliente = Client.create(name: 'Juan',
+                        surname: 'Carlos',
+                        gender: 'Hombre',
+                        dni: 123456,
+                        birthdate: '2015-09-09',
+                        cui: 9999999,
+                        contacts_attributes: [ source: 'skype', value: 'juan.carlos' ]
+                        )
+
+    assert_not_empty cliente.contacts
+  end
+
+  test "borrado CON facturas emitidas" do #deberian borrarse las facturas al eliminar el cliente
+    @client.destroy
+    assert_equal 0, @client.bills.count
+  end
+
+  test "borrado SIN facturas emitidas" do #deberian eliminarse los cntactos al eliminar el cliente
+    @client.destroy
+    assert_equal 0, @client.contacts.count
+  end
+
 end
