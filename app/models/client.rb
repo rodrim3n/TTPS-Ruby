f7d12cd25d94ce17ad2f0fd1aa82d81e49a89354
Rodrigo Otero <rodri.m3n@gmail.com>
2015-12-16 04:09:41 -0300

    adds dni validations to client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,10 +1,16 @@
 class Client < ActiveRecord::Base
   has_many :contacts, :dependent => :destroy
-  accepts_nested_attributes_for :contacts, allow_destroy: true,
-                reject_if: lambda { |attr| attr['value'].blank? }
-
   has_many :bills, :dependent => :destroy
 
   validates :name, :surname, :gender, :dni, :birthdate, :cui, presence: true
+
   validates :cui, :dni, uniqueness: true
+
+  validates :dni, numericality: true
+
+  validates_associated :contacts
+
+  accepts_nested_attributes_for :contacts, allow_destroy: true,
+                reject_if: lambda { |attr| attr['value'].blank? }
+
 end
